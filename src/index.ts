aeadd429c60cfcf9a13518236ba871df4db92daf
v4n3ss4ms <v4n3ss4ms@gmail.com>
2024-09-06 15:04:50 +0200

    refactor: improve extensions, fix scss extension

--- src/index.ts
+++ src/index.ts
@@ -25,15 +25,11 @@ const FrameworkOptionsMap: Record<FrameworkOptionsType, FrameworkOptionsType> = 
   styles: 'styles',
 };
 
-type FrameworkInfo = {
-  name: FrameworkType;
-  createComponent: (componentName: string, options: FrameworkOptionsType[]) => void;
-};
+// type FrameworkInfo = {
+//   name: FrameworkType;
+//   createComponent: (componentName: string, options: FrameworkOptionsType[]) => void;
+// };
 
-const FrameworkSettings: Record<FrameworkType, FrameworkInfo> = {
-  React: { name: 'React', createComponent: createReactComponent },
-  Angular: { name: 'Angular', createComponent: createAngularComponent },
-};
 
 function getComponentSettings(name: string) {
   const className = toPascalCase(name);
@@ -47,8 +43,48 @@ function getComponentSettings(name: string) {
   }
 }
 
-function promptForComponent() {
+const FrameworkSettings: Record<FrameworkType, any> = { // TODO: remove any
+  React: {
+    name: 'React',
+    templates: {
+      component: {
+        template: reactComponentTemplate,
+        extension: '.component.tsx',
+      },
+      style: {
+        template: reactStyleTemplate,
+        extension: '.module.scss',
+      },
+      test: {
+        template: reactTestTemplate,
+        extension: '.component.test.tsx',
+      },
+    },
+  },
+  Angular: {
+    name: 'Angular',
+    templates: {
+      component: {
+        template: angularComponentTemplate,
+        extension: '.component.ts',
+      },
+      style: {
+        template: angularStyleTemplate,
+        extension: '.component.scss',
+      },
+      test: {
+        template: angularTestTemplate,
+        extension: '.component.spec.ts',
+      },
+      html: {
+        template: angularHtmlTemplate,
+        extension: '.component.html',
+      },
+    },
+  },
+};
 
+function promptForComponent() {
   return inquirer.prompt([
     {
       type: 'input',
@@ -74,52 +110,6 @@ function promptForComponent() {
   ]);
 }
 
-function createReactComponent(componentName: string, options: FrameworkOptionsType[]) {
-  const { className, fileName, componentDir } = getComponentSettings(componentName);
-
-  const componentContent = reactComponentTemplate(className);
-  const testContent = reactTestTemplate(className, fileName);
-  const styleContent = reactStyleTemplate(fileName);
-
-  fs.ensureDirSync(componentDir);
-
-  fs.writeFileSync(path.join(componentDir, `${fileName}.component.tsx`), componentContent);
-
-  if (options.includes(FrameworkOptionsMap.styles)) {
-    fs.writeFileSync(path.join(componentDir, `${fileName}.module.scss`), styleContent);
-  }
-
-  if (options.includes(FrameworkOptionsMap.tests)) {
-    fs.writeFileSync(path.join(componentDir, `${fileName}.component.test.tsx`), testContent);
-  }
-
-  successCreationLog(className, componentDir);
-}
-
-function createAngularComponent(componentName: string, options: FrameworkOptionsType[]) {
-  const { className, fileName, componentDir } = getComponentSettings(componentName);
-
-  const componentContent = angularComponentTemplate(className, fileName);
-  const templateContent = angularHtmlTemplate(className);
-  const styleContent = angularStyleTemplate();
-  const testContent = angularTestTemplate(className, fileName);
-
-  fs.ensureDirSync(componentDir);
-
-  fs.writeFileSync(path.join(componentDir, `${fileName}.component.ts`), componentContent);
-  fs.writeFileSync(path.join(componentDir, `${fileName}.component.html`), templateContent);
-
-  if (options.includes(FrameworkOptionsMap.styles)) {
-    fs.writeFileSync(path.join(componentDir, `${fileName}.module.scss`), styleContent);
-  }
-
-  if (options.includes(FrameworkOptionsMap.tests)) {
-    fs.writeFileSync(path.join(componentDir, `${fileName}.component.spec.ts`), testContent);
-  }
-
-  successCreationLog(className, componentDir);
-}
-
 function successCreationLog(componentClassName: string, componentDir: string) {
   console.log(`Component ${componentClassName} created successfully at ${componentDir}`);
 }
@@ -128,11 +118,43 @@ async function createComponent() {
   const { componentName, framework, options } = await promptForComponent();
 
   const frameworkKey = framework as FrameworkType;
-  const frameworkOptions = options as FrameworkOptionsType[];
+
+  const { className, fileName, componentDir } = getComponentSettings(componentName);
+
   const frameworkSettings = FrameworkSettings[frameworkKey];
 
-  frameworkSettings.createComponent(componentName, frameworkOptions);
+  fs.ensureDirSync(componentDir);
+
+  const componentContent = frameworkSettings.templates.component.template(className, fileName);
+  fs.writeFileSync(path.join(componentDir, `${fileName}${frameworkSettings.templates.component.extension}`), componentContent);
+
+  if (options.includes(FrameworkOptionsMap.styles)) {
+    const styleContent = frameworkSettings.templates.style.template(fileName);
+    fs.writeFileSync(path.join(componentDir, `${fileName}${frameworkSettings.templates.style.extension}`), styleContent);
+  }
+
+  if (options.includes(FrameworkOptionsMap.tests)) {
+    const testContent = frameworkSettings.templates.test.template(className, fileName);
+    fs.writeFileSync(path.join(componentDir, `${fileName}${frameworkSettings.templates.test.extension}`), testContent);
+  }
+
+  if (frameworkSettings.templates.html) {
+    const htmlContent = frameworkSettings.templates.html.template(className);
+    fs.writeFileSync(path.join(componentDir, `${fileName}${frameworkSettings.templates.html.extension}`), htmlContent);
+  }
+
+  successCreationLog(className, componentDir);
 }
+
+function writeFile(componentName: string) {
+  const { className, fileName, componentDir } = getComponentSettings(componentName);
+}
+
+
+
+
+
+
 
 const program = new Command();
 
